a1f0c0bf0987f592ed0858e710f14b293d1dcc35
mj <minai621@naver.com>
2024-08-27 17:39:35 +0900

    chore: apply absolute path in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -5,12 +5,17 @@ const config: Config = {
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "@constants/(.*)": "<rootDir>/src/constants/$1",
+    "@utils/(.*)": "<rootDir>/src/utils/$1",
+    "@types/(.*)": "<rootDir>/src/types/$1",
+    "@detectors/(.*)": "<rootDir>/src/detectors/$1",
   },
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
   },
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
 
